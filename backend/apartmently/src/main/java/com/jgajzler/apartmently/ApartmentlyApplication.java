7868ae78eea07a75b441175bbfd1428d317d2c6a
sunsrisemystery <gasia36@gmail.com>
2021-03-22 17:33:05 +0100

    Added files for Spring Boot backend

---
+++ backend/apartmently/src/main/java/com/jgajzler/apartmently/ApartmentlyApplication.java
@@ -0,0 +1,13 @@
+package com.jgajzler.apartmently;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentlyApplication.class, args);
+	}
+
+}
---
+++ backend/apartmently/src/test/java/com/jgajzler/apartmently/ApartmentlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgajzler.apartmently;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
